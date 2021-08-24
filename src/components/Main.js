88c73fa8f7ad0dcb770ed9963fb59b13958514ca
KirillBiglove <79764768+KirillBiglove@users.noreply.github.com>
2021-08-25 01:24:28 +0300

    feat: takoy jesti ja ne ispitival nikogda - priem dannih v profil reazlizovan

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import { useState, useEffect} from 'react';
 import profileAvatar from '../images/cousteau.jpg'
 import addButton from '../images/Vector_plus.svg'
 import api from '../utils/Api';
 
 
-function Main({ onEditProfile, onAddPlace, onEditAvatar, userName, userDescription, userAvatar  }) {
+function Main({ onEditProfile, onAddPlace, onEditAvatar }) {
+
+    const [ userName, setUserName ] = useState('');
+    const [ userDescription, setUserDescription ]= useState('');
+    const [ userAvatar, setUserAvatar ] = useState('');
+    
+    useEffect(() => {
+        api.editProfile( setUserName, setUserDescription)
+        .then((props) =>{
+            setUserName(props.name)
+            setUserDescription(props.about)
+            setUserAvatar(props.avatar)    
+            })
+    }, [])
+    
 
     return (
         <main>
         <section className="profile page__profile">
             <div className="profile__avatar-change-icon">
-                <img className="profile__avatar" style={{ backgroundImage: `url(${userAvatar})` }} src={profileAvatar} onClick={onEditAvatar} alt="Аватар пользователя"></img>
+                <img className="profile__avatar" src={profileAvatar} style={{ backgroundImage: `url(${userAvatar})` }}  onClick={onEditAvatar} alt="Аватар пользователя"></img>
             </div>
             <div className="profile__info">
                 <div className="profile__info-container">
@@ -20,7 +34,7 @@ function Main({ onEditProfile, onAddPlace, onEditAvatar, userName, userDescripti
                 <p className="profile__subtitle">{userDescription}</p>
             </div>
             <button className="profile__add-button link" onClick={onAddPlace} type="button">
-            <img className="profile__rectangle-plus"  src={addButton} alt="кнопка добавить"></img>
+            <img className="profile__rectangle-plus" src={addButton} alt="кнопка добавить"></img>
             </button>
         </section>
 
